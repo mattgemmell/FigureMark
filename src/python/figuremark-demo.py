9d4687ab4d09ef8271a058c8281bb26d7ac3da9a
mattgemmell <matt.gemmell@gmail.com>
2025-06-30 14:01:19 +0100

    Fixed fragility in Python demo script.

--- src/python/figuremark-demo.py
+++ src/python/figuremark-demo.py
@@ -2,23 +2,25 @@ import os
 import sys
 from figuremark import figuremark
 
-filename = "../../example/figuremark-example.md"
+file_path = "../../example/figuremark-example.md"
+this_script_path = os.path.abspath(os.path.expanduser(sys.argv[0]))
+file_path = os.path.join(os.path.dirname(this_script_path), file_path)
+
 if len(sys.argv) > 1:
 	# If there's an argument, assume it's a Markdown file path.
-	filename = sys.argv[1]
+	file_path = sys.argv[1]
 
 try:
 	# Open and read file.
-	filename = os.path.abspath(filename)
-	input_file = open(filename, 'r')
+	input_file = open(os.path.abspath(file_path), 'r')
 	file_contents = input_file.read()
 	input_file.close()
 	
 	# Convert any FigureMark blocks.
 	file_contents = figuremark.convert(file_contents)
-	
+	sys.exit(0)
 	# Write out result to "-converted" file alongside original.
-	basename, sep, ext = filename.partition(".")
+	basename, sep, ext = file_path.partition(".")
 	output_filename = f"{basename}-converted{sep}{ext}"
 	output_file = open(output_filename, 'w')
 	output_file.write(file_contents)
